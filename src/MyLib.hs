e84a9616fd25e9624cfb9b9c5363e30a289ad6ca
monax <>
2023-10-26 15:13:41 +0000

    Add header file and basic bindings.
    FossilOrigin-Name: 094fabcf9abb711b4b35bff86696c0832fe3089467d5a0e172b139b33799cbf8

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,44 @@
-module MyLib (someFunc) where
+{-# LANGUAGE CApiFFI #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module MyLib (
+  cozoOpenDB,
+  cozoCloseDB,
+  cozoRunQuery,
+  cozoImportRelations,
+  cozoExportRelations,
+  cozoBackup,
+  cozoRestore,
+  cozoImportFromBackup,
+  cozoFreeStr,
+) where
+
+import Foreign.C.Types
+import Foreign.Ptr
+
+foreign import capi "cozo_c.h cozo_open_db"
+  cozoOpenDB :: Ptr CChar -> Ptr CChar -> Ptr CChar -> Ptr CInt -> IO ()
+
+foreign import capi "cozo_c.h cozo_close_db"
+  cozoCloseDB :: CInt -> IO CBool
+
+foreign import capi "cozo_c.h cozo_run_query"
+  cozoRunQuery :: CInt -> Ptr CChar -> Ptr CChar -> CBool -> IO (Ptr CChar)
+
+foreign import capi "cozo_c.h cozo_import_relations"
+  cozoImportRelations :: CInt -> Ptr CChar -> IO (Ptr CChar)
+
+foreign import capi "cozo_c.h cozo_export_relations"
+  cozoExportRelations :: CInt -> Ptr CChar -> IO (Ptr CChar)
+
+foreign import capi "cozo_c.h cozo_backup"
+  cozoBackup :: CInt -> Ptr CChar -> IO (Ptr CChar)
+
+foreign import capi "cozo_c.h cozo_restore"
+  cozoRestore :: CInt -> Ptr CChar -> IO (Ptr CChar)
+
+foreign import capi "cozo_c.h cozo_import_from_backup"
+  cozoImportFromBackup :: CInt -> Ptr CChar -> IO (Ptr CChar)
+
+foreign import capi "cozo_c.h cozo_free_str"
+  cozoFreeStr :: Ptr CChar -> IO ()
