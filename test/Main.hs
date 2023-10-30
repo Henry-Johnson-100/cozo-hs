39bb1285b238ba39643368fbc9c149dfaae68ae6
monax <>
2023-10-30 16:51:51 +0000

    Add a test for exporting in one connection and importing in another.
    FossilOrigin-Name: 888db750e29bdd57ba611576352c280ca262a7494b1f86578dcb83a889ce957a

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-typed-holes #-}
 
-import Control.Exception (throwIO)
+import Control.Exception (bracket, throwIO)
 import Database.Cozo
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -10,6 +11,7 @@ main =
   defaultMain
     . testGroup "cozo-hs Tests"
     $ [ connectionTests
+      , exportAndImportTests
       ]
 
 connectionTests :: TestTree
@@ -22,3 +24,36 @@ connectionTests =
         >>= close
         >>= assertBool "Database was already closed or did not exist."
     ]
+
+exportAndImportTests :: TestTree
+exportAndImportTests =
+  testGroup
+    "Export and Import Tests"
+    [ testCase "Can export and import" $ do
+        e <-
+          testWithConnection
+            $ \c -> do
+              _ <-
+                runQuery
+                  c
+                  "?[foo] <- [['foo']]; \
+                  \:create Foo{foo:String}"
+                  empty
+                  >>= either throwIO pure
+              exportRelations c ["Foo"] >>= either throwIO pure
+        (CozoResult (Right o)) <- testWithConnection $ \c -> do
+          _ <- runQuery c ":create Foo{foo:String}" empty >>= either throwIO pure
+          _ <- importRelations c e
+          runQuery c "?[foo] := *Foo{foo}" empty >>= either throwIO pure
+
+        assertEqual
+          "Expecting a single relation to be exported."
+          [[String "foo"]]
+          . cozoOkayRows
+          $ o
+    ]
+
+testWithConnection =
+  bracket
+    (open "mem" "" "{}" >>= either throwIO pure)
+    ((() <$) . close)
